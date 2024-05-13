21951af885446e4fbb6fb50fccc34189bf3e76b4
Sachin Agrawal <95291400+SachinSAgrawal@users.noreply.github.com>
2024-05-13 18:17:38 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,24 +1,40 @@
 # Board Breaker 
 
-## Goal
-Two expert Tae-kwon-do fighters duel for an epic battle lasting one minute. Break the most boards in one minute to become the Kung Fu Fighting Champion. Good luck!
+## About
+Two expert Tae-kwon-do fighters duel for an epic battle lasting one minute. Break the most boards in one minute to become the Kung Fu Fighting Champion. This 2D game is a first-of-its-kind and was coded in Ruby using the Gosu library. If you like this game, I would appreciate if you starred it or even shared it with your friends.
 
 ## Instructions
-```
+* Navigate your character (see keys below) to collect boards
+* Boards will randomly appear on the screen
+* Each board collected earns you one point
+* The game lasts for a total of one minute
+* The player with the highest score at the end of the game wins
+
 Player 1: Arrows
-↓ - moves down
-↑ - moves up
-← - moves left
-→ - moves right
-Player 2: WASD
-Key S - moves down
-Key W - moves up
-Key A - moves left
-Key D - moves right
+```
+↑ - move forward
+← - rotate left
+↓ - move backward
+→ - rotate right
 ```
 
+Player 2: WASD
+```
+W - move forward
+A - rotate left
+S - move backward
+D - rotate right
+```
+
+## Installation
+* Make sure you have Ruby installed on your system
+* Install the Gosu gem with `gem install gosu`
+* Clone this repository or download the game files
+* Navigate to the game directory in your terminal
+* Run the game by typing `ruby mainCode.rb`
+
 ## Permissions
-I am not the original owner of this code. However, the original owner has given me permission to edit and redistribute it with some limitations.
+I am not the original owner of this code. However, they have given me permission to edit and redistribute it.
 
 ## Contributors
 Sachin Agrawal: I'm a self-taught programmer who knows many languages and I'm into app, game, and web development. For more information, check out my website or Github profile. If you would like to contact me, my email is [github@sachin.email](mailto:github@sachin.email).
--- mainCode.rb
+++ mainCode.rb
@@ -1,7 +1,8 @@
-# imports
+# Imports
 require 'rubygems'
 require 'gosu'
 
+# Module defining drawing order for game elements
 module ZOrder
   Background = 0
   Boards = 1
@@ -9,31 +10,38 @@ module ZOrder
   UI = 4
 end
 
+## GAME WINDOW
 class GameWindow < Gosu::Window
-  # set everything up
   def initialize
+    # Initialize everything
     $time = 0
     @minutes = 0
     super(640,480, false)
     self.caption = "Board Breaker"
     @ButtonPress = false
     @ButtonPressTime = 0
-    @background_image = Gosu::Image.new("media/TKD_Background.png")
-    @instructions_image = Gosu::Image.new("media/Instructions.png")
+    # Load images and sounds
+    @background_image = Gosu::Image.new("media/background.png")
+    @instructions_image = Gosu::Image.new("media/instructions.png")
     @music = Gosu::Sample.new("media/kungfu.mp3")
     @music.play(volume = 0.03, speed = 1, looping = true)
+    # Create player objects and set positions
     @tkd_person_1 = Player.new
     @tkd_person_2 = Player.new
     @tkd_person_1.warp(530, 380)
     @tkd_person_2.warp(100, 100)
+    # Load animation tiles for boards
     @board_animation = Gosu::Image::load_tiles("media/board.png", 25, 25)
+    # Create arrays for storing boards and persons
     @boards = Array.new
     @person = Array.new
     @font = Gosu::Font.new(20)
   end
-  # player 1 and 2 controls
+  
+  ## PLAYER CONTROLS
   def update
     $time += 1
+    # Let player one use arrow keys
     if Gosu::button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft
       @tkd_person_1.turn_left
     end
@@ -46,6 +54,7 @@ class GameWindow < Gosu::Window
     if Gosu::button_down? Gosu::KbDown or Gosu::button_down? Gosu::GpButton0
       @tkd_person_1.deaccelerate
     end
+    # Let player two use WASD
     if Gosu::button_down? Gosu::KbA or Gosu::button_down? Gosu::GpLeft
       @tkd_person_2.turn_left
     end
@@ -58,28 +67,33 @@ class GameWindow < Gosu::Window
     if Gosu::button_down? Gosu::KbS or Gosu::button_down? Gosu::GpButton0
       @tkd_person_2.deaccelerate
     end
+    # Move players and handle board collection
     @tkd_person_1.move
     @tkd_person_2.move
     @tkd_person_1.collect_boards(@boards)
     @tkd_person_2.collect_boards(@boards)
-    # spawn boards 7% of time and if less than 25 on screen
+    # Generate new boards 7% of time and if there are less than 25 on screen
     if rand(100) < 7 and @boards.size < 25
       @boards.push(Board.new(@board_animation))
     end
   end
-  # main animations
+
+  # ANIMATIONS
   def draw
+    # Draw background image
     @background_image.draw(0, 0, ZOrder::Background)
     @seconds = ($time-@ButtonPressTime)/60
     @minutes = Time.at(@seconds).utc.strftime("%M:%S")
     if @minutes == "00:00"
       @boards.clear
     end
-    # before one minute
+    # Before one minute
     if @minutes < "01:00"
+      # Animate players and draw boards
       @tkd_person_1.animate
       @tkd_person_2.animate
       @boards.each{ |board| board.draw}
+      # Draw each player's score and the time with a shadow
       @font.draw("Time: #{@minutes}", 266, 16, ZOrder::UI, 1.0, 1.0, 0xff_000000)
       @font.draw("Time: #{@minutes}", 265, 15, ZOrder::UI, 1.0, 1.0, 0xff_00d900)
       @font.draw("Player 1 Score: #{@tkd_person_1.score}", 91, 16, ZOrder::UI, 1.0, 1.0, 0xff_000000)
@@ -87,8 +101,9 @@ class GameWindow < Gosu::Window
       @font.draw("Player 1 Score: #{@tkd_person_1.score}", 90, 15, ZOrder::UI, 1.0, 1.0, 0xff_1259ff)
       @font.draw("Player 2 Score: #{@tkd_person_2.score}", 400, 15, ZOrder::UI, 1.0, 1.0, 0xff_ff0000)
     end
-    # after one minute
+    # After one minute
     if @minutes >= "01:00"
+      # Handle end game conditions and draw messages of winner
       if @tkd_person_1.score == @tkd_person_2.score
         @font.draw("TIE!!!", 301, 211, ZOrder::UI, 1.0, 1.0, 0xff_000000)
         @font.draw("TIE!!!", 300, 210, ZOrder::UI, 1.0, 1.0, 0xff_d900ff)
@@ -101,28 +116,35 @@ class GameWindow < Gosu::Window
         @font.draw("PLAYER 2 WON!!!", 249, 211, ZOrder::UI, 1.0, 1.0, 0xff_000000)
         @font.draw("PLAYER 2 WON!!!", 248, 210, ZOrder::UI, 1.0, 1.0, 0xff_ff0000)
       end
+    # Draw end game instructions
     @font.draw("Press escape to quit.", 241, 236, ZOrder::UI, 1.0, 1.0, 0xff_000000)
     @font.draw("Press space to play again.", 222, 261, ZOrder::UI, 1.0, 1.0, 0xff_000000)
     @font.draw("Press escape to quit.", 240, 235, ZOrder::UI, 1.0, 1.0, 0xff_00d900)
     @font.draw("Press space to play again.", 221, 260, ZOrder::UI, 1.0, 1.0, 0xff_00d900)
     @boards.clear
     end
+    # Draw instructions image if button not pressed
     if @ButtonPress == false
       @instructions_image.draw(0,0, 10)
     end
   end
-  # button presses
+
+  # KEY PRESSES
   def button_down(id)
+    # Record time of button press if it hasn't occurred yet
     if @ButtonPress == false
       @ButtonPressTime = $time
     end
     @ButtonPress = true
+    # Calculate elapsed time in seconds since button press
     @seconds = ($time-@ButtonPressTime)/60
     @minutes = Time.at(@seconds).utc.strftime("%M:%S")
     if @minutes > "01:00"
+      # Close the game if 'Escape' is pressed
       if id == Gosu::KbEscape
           close
       end
+      # Otherwise reset the game if 'Space' is pressed
       if id == Gosu::KbSpace
         @ButtonPressTime = $time
         @tkd_person_1.clear_score
@@ -136,17 +158,20 @@ class GameWindow < Gosu::Window
   end
 end
 
-# update player movements
+## PLAYER CLASS
 class Player
+  # Initialize player attributes
   def initialize
-    @image = Gosu::Image.new("media/tkd_image.bmp")
-    @animation = Gosu::Image::load_tiles("media/TAEKWONDO_GUY.png", 50, 64)
+    @image = Gosu::Image.new("media/taekwondo.bmp")
+    @animation = Gosu::Image::load_tiles("media/taekwondo.png", 50, 64)
     @x = @y = @vel_x = @vel_y = @angle = 0.0
     @score = 0
   end
+  # Set player position
   def warp(x, y)
     @x, @y = x, y
   end
+  # Rotate the player left or right
   def turn_left
     @angle -= 4.5
   end
@@ -156,6 +181,7 @@ class Player
   def clearangle
     @angle = 0
   end
+  # Accelerate or decelerate player in current direction
   def accelerate
     @vel_x = Gosu::offset_x(@angle,3)
     @vel_y = Gosu::offset_y(@angle, 3)
@@ -164,14 +190,17 @@ class Player
     @vel_x = Gosu::offset_x(@angle, -3)
     @vel_y = Gosu::offset_y(@angle, -3)
   end
+  # Move player based on velocity
   def move
     @x += @vel_x
     @y += @vel_y
+    # Wrap player around screen
     @x %= 640
     @y %= 480
     @vel_x *= 0.95
     @vel_y *= 0.95
   end
+  # Draw player image and animation frames rotated according to angle
   def draw
     @image.draw_rot(@x, @y, 1, @angle)
   end
@@ -185,29 +214,37 @@ class Player
   def clear_score
     @score = 0
   end
+  # Check for collision with boards and update score
   def collect_boards(boards)
     if boards.reject! {|board| Gosu::distance(@x, @y, board.x, board.y) < 35 }
+      # Increment score if player collides with a board
       @score += 1
     end
   end
 end
 
-# other stuff
+## BOARD CLASS
 class Board
   attr_reader :x, :y
 
   def initialize(animation)
+    # Initialize board attributes
     @animation = animation
     @color = Gosu::Color.argb(0xff_00ff00)
+    # Randomize initial position within screen bounds
     @x = ( rand * 570 ) + 35
     @y = ( rand * 410 ) + 35
   end
 
+  # Draw board
   def draw
     img = @animation[1 / 100 % @animation.size]
     img.draw_rot(@x,@y,2, 90)
   end
 end
 
+# Create game window
 window = GameWindow.new
+
+# Start the game loop
 window.show
